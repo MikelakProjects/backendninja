2e3a3291a5a52c94e181d558ef8a7735fa803aa9
malmazan@gfi.es <malmazan@gfi.es>
2018-04-16 16:33:19 +0200

    más pruebas para git

--- src/main/java/com/udemy/backendninja/controller/ErrorsController.java
+++ src/main/java/com/udemy/backendninja/controller/ErrorsController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ErrorsController {
 	
-	public static final String ISE_VIEW = "error/500";
+	public static final String ISE_VIEW_JARL = "error/500";
 	
 	@ExceptionHandler(Exception.class)
 	public String showInternalServerError() {
-		return ISE_VIEW;
+		return ISE_VIEW_JARL;
 	}
 
 }
